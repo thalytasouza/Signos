5e5489f64a99c3806e4c61f934b93b2c4c64dfe4
Thalyta Souza <mayerflot@gmail.com>
2023-11-19 12:08:37 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Thalytinha on 19/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
